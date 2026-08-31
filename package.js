66dc60d7b57236ef8ec9255a370cf337e27219fa
nuruzzamannahid-dot <nuruzzaman.nahid@carrybee.com>
2026-09-01 00:46:19 +0600

    Create package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "merchant-dod-dashboard",
+  "version": "1.0.0",
+  "private": true,
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "@libsql/client": "^0.14.0"
+  },
+  "engines": {
+    "node": ">=18"
+  }
+}
